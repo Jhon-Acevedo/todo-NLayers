399cc33a028341d5618f13e31f8aee6c88168e5d
Jhon-Acevedo <jhon.acevedo01@uptc.edu.co>
2024-08-22 17:54:32 -0500

    fix: error cors

--- src/main/java/com/mindhub/todolist/configuration/SwaggerRedirectConfig.java
+++ src/main/java/com/mindhub/todolist/configuration/SwaggerRedirectConfig.java
@@ -1,6 +1,8 @@
 package com.mindhub.todolist.configuration;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -11,5 +13,19 @@ public class SwaggerRedirectConfig implements WebMvcConfigurer {
     public void addViewControllers(ViewControllerRegistry registry) {
        registry.addRedirectViewController("/", "/swagger-ui.html");
        registry.addRedirectViewController("/swagger", "/swagger-ui.html");
+    }
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("https://specified-danell-jhon-acevedo-9280d883.koyeb.app")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedHeaders("*")
+                        .allowCredentials(true);
+            }
+        };
     }
 }
